0077592f5f66fd58a4bdc4ad51a931c691f41a65
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-11-24 01:11:56 -0600

    Updated server.mjs file

--- server.mjs
+++ server.mjs
@@ -2,35 +2,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.mjs';
-import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.mjs';
 import playerRoutes from './routes/playerRoutes.mjs';
 import dungeonRoutes from './routes/dungeonRoutes.mjs';
 import { errorHandler } from './middleware/errorHandler.mjs';
-
 
 
 //Initialization
 dotenv.config();
 connectDB();
 
+
 //Variable declaration
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 //MiddleWare
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ extended: false }));
 app.use(errorHandler);
+
 
 //Routes
 app.use('/api/players', playerRoutes);
 app.use('/api/dungeons', dungeonRoutes);
 
+
 //Listener
-app.listen(PORT, (reqs, resp)=>{
+app.listen(PORT, (reqs, resp) => {
 
     console.log(`Server is running on PORT: ${PORT}`);
 
